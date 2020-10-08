bc2a5b39091973135ae3f233540d0834311c3a7c
scxiong <1554960184@qq.com>
2020-10-08 23:39:24 +0800

    获取accessToken

--- src/main/java/com/buckwheat/community/controller/IndexController.java
+++ src/main/java/com/buckwheat/community/controller/IndexController.java
@@ -1,9 +1,7 @@
-package com.buckwheat.community.controller;
+package com.buckwheat.community.provider.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
